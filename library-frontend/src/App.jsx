bff03826685d2cbe8accdb7c1bf12c772aa69f71
siddh <siddhantkumar7488@gmail.com>
2025-12-04 17:31:09 +0530

    Adding : Navbar, WelcomeOffer, Home, Booking , DashBoard, library cards

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import WelcomeOffer from "./components/WelcomeOffer";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking"; // We will create this next
+import Dashboard from "./pages/Dashboard"; 
+function App() {
   return (
-    <h1 className="text-7xl font-bold underline text-amber-200 bg-amber-700 m-12 text-center">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300 font-sans">
+        <Navbar />
+        <WelcomeOffer />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<Booking />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;

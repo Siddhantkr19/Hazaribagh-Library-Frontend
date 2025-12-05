84fadcaf1bb5604450818b61eb1e91f49fa1e91b
siddh <siddhantkumar7488@gmail.com>
2025-12-05 17:56:29 +0530

    Remove Welcome offer form the APP.jsx to the home page .

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
-import WelcomeOffer from "./components/WelcomeOffer";
+
 import Home from "./pages/Home";
-import Booking from "./pages/Booking"; // We will create this next
+import Booking from "./pages/Booking"; 
 import Dashboard from "./pages/Dashboard"; 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300 font-sans">
         <Navbar />
-        <WelcomeOffer />
         
         <Routes>
           <Route path="/" element={<Home />} />

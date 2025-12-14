c9e16595d93b08fa89328ac85ae25964506dc378
siddh <siddhantkumar7488@gmail.com>
2025-12-14 18:00:45 +0530

    feat : Add withCredentiails : true  to connect  properly with the backend  .

--- library-frontend/src/services/api.js
+++ library-frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/auth',
+  baseURL: 'http://localhost:8080/api', // General Base URL
+  withCredentials: true, // [CRITICAL] This sends the HttpOnly Cookie to the Backend
   headers: {
     'Content-Type': 'application/json',
   },

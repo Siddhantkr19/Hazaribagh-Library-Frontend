bbb2686ad54200652183cad5900e6f52abb04587
siddh <siddhantkumar7488@gmail.com>
2025-12-28 20:53:14 +0530

    update: admin.api

--- library-frontend/src/services/adminApi.js
+++ library-frontend/src/services/adminApi.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-
-const adminApi = axios.create({
+const api = axios.create({
   baseURL: 'https://libhub-6izs.onrender.com/api', 
+  withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true 
 });
 
-export default adminApi;
+export default api
